1be00c197d184022c05e6291563f0bd14be33ff1
TheNapse <126022850+TheNapse@users.noreply.github.com>
2026-02-20 03:51:51 +0100

    Update EmojiTest for new terminal-focused constants

--- tests/EmojiTest.php
+++ tests/EmojiTest.php
@@ -11,10 +11,15 @@ final class EmojiTest extends TestCase
 {
     public function testConstantsExist(): void
     {
-        $this->assertNotEmpty(Emoji::HEART);
-        $this->assertNotEmpty(Emoji::EYES);
-        $this->assertNotEmpty(Emoji::GRINNING);
         $this->assertNotEmpty(Emoji::CHECKMARK_OK);
         $this->assertNotEmpty(Emoji::CHECKMARK_NOK);
+        $this->assertNotEmpty(Emoji::WARNING);
+        $this->assertNotEmpty(Emoji::INFO);
+        $this->assertNotEmpty(Emoji::ARROW_RIGHT);
+        $this->assertNotEmpty(Emoji::ROCKET);
+        $this->assertNotEmpty(Emoji::HOURGLASS);
+        $this->assertNotEmpty(Emoji::LOCK);
+        $this->assertNotEmpty(Emoji::GEAR);
+        $this->assertNotEmpty(Emoji::SPARKLE);
     }
 }
